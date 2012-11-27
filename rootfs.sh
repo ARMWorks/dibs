4620cb033d955b43039613c785845ef0d695b15a
Jeff Kent <jakent@gmail.com>
2012-11-27 10:14:13 -0600

    added qemu-system and added vmlinuz-arm kernel

--- rootfs.sh
+++ rootfs.sh
@@ -24,7 +24,8 @@ fi
 show_usage() {
   echo Usage:
   echo "  $0 setup"
-  echo "  $0 shell" 
+  echo "  $0 shell"
+  echo "  $0 qemu"
 }
 
 setup_binfmt() {
@@ -106,7 +107,7 @@ do_debootstrap() {
     cp -n $CACHE/*.deb $ROOT/var/cache/apt/archives
   fi
 
-  debootstrap --variant=minbase --arch $ARCH --include=aptitude $SUITE $ROOT $MIRROR
+  debootstrap --variant=minbase --arch $ARCH --include=aptitude,udev $SUITE $ROOT $MIRROR
   mount -t proc proc $ROOT/proc
 
   sudo -u $SUDO_USER cp -n $ROOT/var/cache/apt/archives/*.deb $CACHE
@@ -114,48 +115,64 @@ do_debootstrap() {
   teardown_root
 }
 
+do_postconfig() {
+  case $SYSTEM in
+    debian)
+      cat << __END__ > $ROOT/etc/apt/sources.list
+deb http://mirrors.kernel.org/debian $SUITE main contrib non-free
+deb-src http://mirrors.kernel.org/debian $SUITE main contrib non-free
+deb http://security.debian.org/ $SUITE main contrib non-free
+deb-src http://security.debian.org/ $SUITE main contrib non-free
+__END__
+      ;;
+    *)
+      ;;
+  esac
 
-#cat << __END__ > $ROOTDIR/etc/apt/sources.list
-#deb http://security.debian.org/ $SUITE/updates main contrib non-free
-#deb-src http://security.debian.org/ $SUITE/updates main contrib non-free
-#deb http://mirrors.kernel.org/debian/ $SUITE main contrib non-free
-#deb-src http://mirrors.kernel.org/debian/ $SUITE main contrib non-free
-#__END__
-#sudo chroot $ROOTDIR apt-get update
+  # set empty root password
+  sed 's/\(root:\)[^:]*\(:\)/\1\2/' $ROOT/etc/shadow
 
-#if [ ! -f "$BUILD/vmlinuz" ]; then
-#  mkdir -p $BUILD/tmp
-#  wget $MIRROR/pool/main/l/linux/linux-image-3.2.0-4-vexpress_3.2.32-1_armhf.deb -O $BUILD/tmp/kernel.deb
-#  dpkg-deb -x $BUILD/tmp/*.deb $BUILD/tmp
-#  sudo -u $SUDO_USER cp $BUILD/tmp/boot/vmlinuz* $BUILD/vmlinuz
-#  rm -r $BUILD/tmp
-#fi
+  # set hostname and hosts file
+  echo $HOSTNAME > $ROOT/etc/hostname
+  cat > $ROOT/etc/hosts << __END__
+127.0.0.1       localhost
+127.0.1.1       $HOSTNAME
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+__END__
+
+  
+}
 
 case $1 in
   setup)
+    setup_root
     do_debootstrap
-
-    #init_binfmt
-    #cp post-install.sh $ROOTDIR
-    #run_target /bin/sh /post-install.sh
-    #rm $ROOTDIR/post-install.sh
+    do_postconfig
+    teardown_root
     ;;
   shell)
 	if [ ! -e $DEVICE ]; then
       echo Device or image does not exist
       exit
     fi
-
     setup_root
     setup_binfmt
     debian_chroot=$ARCH run_target /bin/bash
     teardown_root
     ;;
   qemu)
-    echo Not yet implemented
-    exit
+    sudo -u $SUDO_USER qemu-system-arm -machine vexpress-a9 -cpu cortex-a8 \
+      -sd $DEVICE -kernel $TOP/vmlinuz-arm \
+      -append "root=/dev/mmcblk0 rw rootwait" 
     ;;
   *)
     show_usage
     ;;
 esac
+
