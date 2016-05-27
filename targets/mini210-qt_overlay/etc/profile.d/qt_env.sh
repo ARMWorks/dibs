671d3e3487b239a73609a6b1f396df04545ea4b0
Jason Pruitt <jrspruitt@gmail.com>
2016-05-27 15:48:14 -0700

    Added Mini210 w/Qt config

---
+++ targets/mini210-qt_overlay/etc/profile.d/qt_env.sh
@@ -0,0 +1,10 @@
+export TERM=xterm
+export TSLIB_CALIBFILE=/etc/pointercal
+export TSLIB_CONFFILE=/etc/ts.conf
+export TSLIB_TSDEVICE=/dev/input/event0
+export TSLIB_FBDEVICE=/dev/fb0
+export TSLIB_CONSOLEDEVICE=/dev/tty1
+export TSLIB_PLUGINDIR=/usr/lib/ts
+export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb0
+export QT_QPA_FB_TSLIB=1
+export QT_QPA_GENERIC_PLUGINS=tslib:/dev/input/event0
