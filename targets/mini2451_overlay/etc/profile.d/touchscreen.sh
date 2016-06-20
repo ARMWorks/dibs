75e2fadc7c42d23ddf0a5d07898b9eadde6158e1
Jason Pruitt <jrspruitt@gmail.com>
2016-06-20 01:28:27 -0700

    Updated Mini2451 better support and tslib.

---
+++ targets/mini2451_overlay/etc/profile.d/touchscreen.sh
@@ -0,0 +1,6 @@
+export TSLIB_CALIBFILE=/etc/pointercal
+export TSLIB_CONFFILE=/etc/ts.conf
+export TSLIB_TSDEVICE=/dev/input/event0
+export TSLIB_FBDEVICE=/dev/fb0
+export TSLIB_CONSOLEDEVICE=/dev/tty1
+export TSLIB_PLUGINDIR=/usr/lib/ts
