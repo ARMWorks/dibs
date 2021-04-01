a22ad33e672b88f6e4393b9f7e54f061db7db278
Jeff Kent <jeff@jkent.net>
2021-04-01 00:31:39 -0500

    remove elevation code

--- dibs/__main__.py
+++ dibs/__main__.py
@@ -5,9 +5,6 @@ import dibs.project as project
 import dibs.util as util
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2 and sys.argv[1] == 'sudo':
-        sys.exit(util.call(sys.argv[2]))
-
     if not os.path.exists('config.yaml'):
         project.defconfig('mini210')
     project.build()
