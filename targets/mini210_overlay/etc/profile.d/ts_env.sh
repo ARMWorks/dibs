5980eb393a9ef30b7113a0a09951a64c873df4d9
Jason Pruitt <jrspruitt@gmail.com>
2022-03-09 13:37:42 -0800

    Updated mini210 for updated FE 3.0.8 Kernel.

--- targets/mini210_overlay/etc/profile.d/ts_env.sh
+++ targets/mini210_overlay/etc/profile.d/ts_env.sh
@@ -1,7 +1,7 @@
 export TERM=xterm
 export TSLIB_CALIBFILE=/etc/pointercal
 export TSLIB_CONFFILE=/etc/ts.conf
-export TSLIB_TSDEVICE=/dev/input/event0
+export TSLIB_TSDEVICE=/dev/touchscreen-1wire
 export TSLIB_FBDEVICE=/dev/fb0
 export TSLIB_CONSOLEDEVICE=/dev/tty1
 export TSLIB_PLUGINDIR=/usr/lib/ts
